237f26025b04ccbc598b3c8faf3bc4ad27f15ace
Muslim Khilalov <hilalov@yandex.ru>
2025-11-06 14:38:36 +0300

    Creating ROOMS and game_state variables

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -3,5 +3,13 @@
 def main():
     print("Первая попытка запустить проект!")
 
+game_state = {
+    'player_inventory': [], # Инвентарь игрока
+    'current_room': 'entrance', # Текущая комната
+    'game_over': False, # Значения окончания игры
+    'steps_taken': 0 # Количество шагов
+}
+  
+
 if __name__ == '__main__':
     main()
